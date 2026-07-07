2bf37935fdafedbaa88b72fb1a5ad06c4c511b83
Bryan Dunlap <bryan.dunlap@outlook.com>
2026-07-06 18:31:14 -0700

    Create Mosaic dashboard shell

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+const frontendPath = path.join(__dirname, "..", "frontend");
+
+app.use(express.static(frontendPath));
+
 app.get("/", (req, res) => {
-    res.send("Project Mosaic is alive.");
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Project Mosaic running on port ${PORT}`);
+  console.log(`Project Mosaic running at http://localhost:${PORT}`);
 });
