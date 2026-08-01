6ccd6cb63be0061bbf4980c573425fc71c518c3e
Bryan Dunlap <bryandunlap@Bryans-MacBook-Pro.local>
2026-08-01 13:54:38 -0700

    Initialize Mosaic provider lifecycle

--- frontend/core/app-core.js
+++ frontend/core/app-core.js
@@ -3,6 +3,15 @@ class MosaicApp {
     constructor() {
         this.eventBus = new MosaicEventBus();
         this.providerManager = new ProviderManager();
+        this.providerRegistry = new ProviderRegistry();
+    }
+
+    start() {
+        registerMosaicProviders(this.providerRegistry);
+        this.providerManager.loadFromRegistry(
+            this.providerRegistry
+        );
+        this.providerManager.start();
     }
 
 }
