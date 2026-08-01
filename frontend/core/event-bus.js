b58cd0c07e179b9543cb3fbe0676426edb703caf
Bryan Dunlap <bryandunlap@Bryans-MacBook-Pro.local>
2026-08-01 12:34:04 -0700

    Add core event bus system

---
+++ frontend/core/event-bus.js
@@ -0,0 +1,37 @@
+class MosaicEventBus {
+
+    constructor() {
+        this.subscribers = new Map();
+    }
+
+    subscribe(type, callback) {
+        if (!this.subscribers.has(type)) {
+            this.subscribers.set(type, new Set());
+        }
+
+        this.subscribers.get(type).add(callback);
+
+        return () => this.unsubscribe(type, callback);
+    }
+
+    unsubscribe(type, callback) {
+        const callbacks = this.subscribers.get(type);
+
+        if (!callbacks) return;
+
+        callbacks.delete(callback);
+
+        if (callbacks.size === 0) {
+            this.subscribers.delete(type);
+        }
+    }
+
+    publish(event) {
+        const callbacks = this.subscribers.get(event.type);
+
+        if (!callbacks) return;
+
+        callbacks.forEach((callback) => callback(event));
+    }
+
+}
